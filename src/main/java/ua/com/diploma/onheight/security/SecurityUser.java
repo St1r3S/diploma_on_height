8ef98393b91651d309df8630cac74bf8279267f8
St1r3S <mikmuael.steam@gmail.com>
2023-05-29 00:18:42 +0300

    add in memory auth

---
+++ src/main/java/ua/com/diploma/onheight/security/SecurityUser.java
@@ -0,0 +1,72 @@
+package ua.com.diploma.onheight.security;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ua.com.diploma.onheight.model.user.User;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class SecurityUser implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final List<SimpleGrantedAuthority> authorities;
+    private final boolean isActive;
+
+    public SecurityUser(String username, String password, List<SimpleGrantedAuthority> authorities, boolean isActive) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+        this.isActive = isActive;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+
+    public static UserDetails fromUser(User user) {
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPasswordHash(),
+                true,
+                true,
+                true,
+                true,
+                user.getUserRole().getAuthorities()
+        );
+    }
+}
